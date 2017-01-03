d6426561e681bcefac43b2ffd5aaecea997be3ac
mnicdao01 <marknicdao@Marks-MacBook-Pro.local>
2017-01-03 14:18:03 +0700

    Fixed: AWS Region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,8 @@ Rails.application.configure do
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_host_name: ENV.fetch('S3_URL')
+    s3_region: ENV.fetch('AWS_REGION'),
+    s3_host_name: ENV.fetch('S3_URL'),
   }
 }
 end
