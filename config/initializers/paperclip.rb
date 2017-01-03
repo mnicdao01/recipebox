809a178d8ae67f207b163bf22ef9b062435fedb0
mnicdao01 <marknicdao@Marks-MacBook-Pro.local>
2017-01-03 15:31:57 +0700

    TRIAL: Manual Region

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,2 @@
-Paperclip::Attachment.default_options[:s3_url] = 's3.recipeboxv1.amazonaws.com'
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3-ap-southeast-1.amazonaws.com'
