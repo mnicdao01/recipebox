cee30fbb47a3b74f7dbf2a886ea150fc5f95e880
mnicdao01 <marknicdao@Marks-MacBook-Pro.local>
2017-01-03 15:23:47 +0700

    Fixed: Deleting of images in S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
     s3_region: ENV.fetch('AWS_REGION'),
-    s3_url: :s3_url,
+    s3_url: :s3_host_name
     s3_host_name: :s3_host_name,
     path: :path,
   }
