5ba29cc45ff061638f16fa528d5adbb5e70f0214
mnicdao01 <marknicdao@Marks-MacBook-Pro.local>
2017-01-03 08:14:03 +0700

    Changes

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,3 @@
-aperclip::Attachment.default_options[:s3_host_name] = 'recipeboxv1.s3.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'recipeboxv1.s3.amazonaws.com'
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
