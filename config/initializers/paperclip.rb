b45d85a93ce7014d8860715fcc701ca0f11dde10
mnicdao01 <marknicdao@Marks-MacBook-Pro.local>
2017-01-03 08:05:41 +0700

    Added: Paperclip.rb correct url

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,1 @@
-Paperclip::Attachment.default_options[:url] = 's3-us-west-2.amazonaws.com'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+Paperclip::Attachment.default_options[:url] = 'http://recipeboxv1.s3.amazonaws.com/'
